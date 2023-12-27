d21ff22c9cb8d14020a0bb947bccc5c9510e248a
mattc-27 <mattcopeland927@gmail.com>
2023-12-27 13:02:38 -0700

    Initial commit 12-27-23

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css'
+
+
+export default function App() {
+
+    return (
+        <div className='main'>
+            <div className='title'>
+                <h1>Hello world</h1>
+            </div>
+        </div>
+    );
+}
